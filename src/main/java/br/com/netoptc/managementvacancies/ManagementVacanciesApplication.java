8fe993258d337374d19dd1fd2d00bbdb00bcc347
João Neto <joaoneto@Joaos-MacBook-Air.local>
2024-04-11 21:11:53 -0300

    first commit

---
+++ src/main/java/br/com/netoptc/managementvacancies/ManagementVacanciesApplication.java
@@ -0,0 +1,13 @@
+package br.com.netoptc.managementvacancies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementVacanciesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementVacanciesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/netoptc/managementvacancies/ManagementVacanciesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.netoptc.managementvacancies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementVacanciesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
