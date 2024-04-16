43e26c48e7299126c72581b2be09ae9e4a8dc4f9
João Neto <joaoneto@Joaos-MacBook-Air.local>
2024-04-15 23:54:17 -0300

    create candidate table

--- src/main/java/br/com/netoptc/managementvacancies/modules/candidate/entity/Candidate.java
+++ src/main/java/br/com/netoptc/managementvacancies/modules/candidate/entity/Candidate.java
@@ -1,16 +1,25 @@
 package br.com.netoptc.managementvacancies.modules.candidate.entity;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Entity(name = "candidate")
 public class Candidate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
     private String name;
     @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço")
     private String username;
@@ -20,4 +29,6 @@ public class Candidate {
     private String password;
     private String description;
     private String curriculum;
+
+    private LocalDateTime createdAt;
 }
